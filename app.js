a060e2f104c16f0a4d0290552393ffba2910d7fe
archiitsharmaa <archit.sharma@paxcom.net>
2022-08-12 00:53:48 +0530

    swagger ui docs added at /api-docs

--- app.js
+++ app.js
@@ -1,79 +1,84 @@
 //imprting express module
-const express = require('express');
+const express = require("express");
 const app = express();
-const req = require('express/lib/request');
+const req = require("express/lib/request");
 
 //importing mongoose for mongodb
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //importing body parser to use json
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 //importing cors value
-const cors = require('cors')
+const cors = require("cors");
 
 //importing route middleware
-const routes = require('./routes/routes');
+const routes = require("./routes/routes");
 
 //importing login module
 const logger = require("./logger");
 
 //importing values for config file
-var PropertiesReader = require('properties-reader');
-var properties = PropertiesReader('config/app.properties');
+var PropertiesReader = require("properties-reader");
+var properties = PropertiesReader("config/app.properties");
+
+const swaggerUi = require("swagger-ui-express"),
+  swaggerDocument = require("./swagger.json");
 
 //main class to run app
 class Server {
+  //constructr
+  constructor() {
+    this.initDB();
+    this.initExpressMiddleware();
+    this.initRoutes();
+    this.start();
+  }
 
-    //constructr
-    constructor() {
-        this.initDB();
-        this.initExpressMiddleware();
-        this.initRoutes();
-        this.start();
+  //method to start the pp
+  start() {
+    try {
+      //listen on a port
+      app.listen(properties.get("severPort"));
+      logger.info(
+        "Server listening at the port " + properties.get("severPort")
+      );
+    } catch (err) {
+      //error handling
+      logger.error(
+        "Error listening at the port " + properties.get("severPort")
+      );
     }
+  }
 
-    //method to start the pp
-    start() {
+  //method calling middlewares
+  initExpressMiddleware() {
+    //cors issue
+    app.use(cors());
+    app.use(bodyParser.json());
+  }
 
-        try {
-            //listen on a port
-            app.listen(properties.get("severPort"));
-            logger.info('Server listening at the port ' + properties.get("severPort"));
-        } catch (err) {
-            //error handling
-            logger.error('Error listening at the port ' + properties.get("severPort"));
-        }
+  //method intializing routes
+  initRoutes() {
+    app.use("/", routes);
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  }
+
+  //method connecting to database
+  initDB() {
+    try {
+      //coonect to DB
+      mongoose.connect(
+        properties.get("DATABASE_PATH"),
+        {
+          ignoreUndefined: true,
+        },
+        () => logger.info("Connected to Database")
+      );
+    } catch (err) {
+      logger.error("Error While Connecting to database");
     }
-
-    //method calling middlewares
-    initExpressMiddleware() {
-        //cors issue
-        app.use(cors())
-        app.use(bodyParser.json());
-    }
-
-    //method intializing routes
-    initRoutes() {
-        app.use('/', routes);
-
-    }
-
-    //method connecting to database
-    initDB() {
-
-        try {
-            //coonect to DB
-            mongoose.connect(properties.get("DATABASE_PATH"), {
-                    ignoreUndefined: true
-                }, () =>
-                logger.info("Connected to Database"));
-        } catch (err) {
-            logger.error('Error While Connecting to database');
-        }
-    }
-
-
+  }
 }
 
 new Server();
--- services/routeService/loginRoutes/refreshToken.js
+++ services/routeService/loginRoutes/refreshToken.js
@@ -1,43 +1,37 @@
 //importing refreshverification services
-const refreshVerify = require('../../tokenSevice/verifyrefreshToken');
+const refreshVerify = require("../../tokenSevice/verifyrefreshToken");
 
 //importing token genration
-const tokenGenrate = require('../../tokenSevice/token');
+const tokenGenrate = require("../../tokenSevice/token");
 
 //importing logger function
 const logger = require("../../../logger");
 
 //method to genrate jwt token and refresh token based on last refresh token
-async function refreshTokenService(req,res){
-    
-    try{
+async function refreshTokenService(req, res) {
+  try {
+    //fetching refreshtoken from request body
+    const { refreshtoken } = req.body;
 
-      //fetching refreshtoken from request body
-        const {refreshtoken} = req.body;
+    //verifying validaity of refresh token
+    const verifyStatus = await refreshVerify(refreshtoken);
 
-        //verifying validaity of refresh token
-          const verifyStatus = await refreshVerify(refreshtoken);
-
-          //exit block if refresh token passed is invalid
-          if(!verifyStatus) {
-            logger.info("Invalid refresh token requested");
-            return res.json({"mssg" : "Invalid refresh token"});
-              
-          }
-          //genrating new jwt token and refresh token
-          else{
-             logger.info("New token genrated using refresh token");
-             return res.json(tokenGenrate(verifyStatus.id, verifyStatus.username));
-              
-          }
-
-       }
-          catch(err){
-           
-           logger.error("Error while requesting token");
-           //error handling
-           res.json({"mssg" : "Error while requesting token"});   
-          }
+    //exit block if refresh token passed is invalid
+    if (!verifyStatus) {
+      logger.info("Invalid refresh token requested");
+      return res.json({ mssg: "Invalid refresh token" });
+    }
+    //genrating new jwt token and refresh token
+    else {
+      logger.info("New token genrated using refresh token");
+      return res.json(tokenGenrate(verifyStatus.id, verifyStatus.username));
+    }
+  } catch (err) {
+    logger.error(err);
+    logger.error("Error while requesting token");
+    //error handling
+    res.json({ mssg: "Error while requesting token" });
+  }
 }
 
 module.exports = refreshTokenService;
